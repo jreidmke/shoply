0a5ad36b42a38fea264dd5fc29ff0bde02272b85
unknown <jreidmke@gmail.com>
2020-12-11 12:46:39 -0600

    completed action modules

---
+++ src/actions.js
@@ -0,0 +1,4 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from './actionTypes';
+
+export const add_to_cart = () => ({ type: ADD_TO_CART });
+export const remove_from_cart = () => ({ type: REMOVE_FROM_CART });
