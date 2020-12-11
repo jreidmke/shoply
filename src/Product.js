a3b7d36dfbb2d43fba5b7ee8cff1ae3ae9408471
unknown <jreidmke@gmail.com>
2020-12-11 10:52:39 -0600

    styling for product component

--- src/Product.js
+++ src/Product.js
@@ -1,3 +1,16 @@
-const Product = ({name, price, description, imgSrc}) => {
-    
+import './Product.css';
+
+const Product = ({name, price, description, image_url, handleRemove, handleAdd, id}) => {
+    return(
+        <div id='productContainer'>
+            <img src={image_url} alt='Product Image' id='productImg'/>
+            <h3>{name}</h3>
+            <h5>Price: {price}</h5>
+            <h6>{description}</h6>
+            <button onClick={()=>handleRemove(id)}>Remove From Cart</button>
+            <button onClick={()=>handleAdd(id)}>Add To Cart</button>
+        </div>
+    )
 }
+
+export default Product;
