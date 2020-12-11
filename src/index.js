2869ffb4bf7e9c3d9707babb966a7b474c45de41
unknown <jreidmke@gmail.com>
2020-12-11 13:04:06 -0600

    added Provider and router

--- src/index.js
+++ src/index.js
@@ -3,19 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import { BrowserRouter, Route } from "react-router-dom";
+import { createStore } from "redux";
+import rootReducer from "./rootReducer";
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-      <BrowserRouter>
-
-
-    <App />
+  <Provider store={store}>
+    <BrowserRouter>
+      <Route path="/" component={App} />
     </BrowserRouter>
-
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
