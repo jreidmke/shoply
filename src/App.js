7cc8834b79d9752077cd0c8fada24222469a8319
unknown <jreidmke@gmail.com>
2020-12-11 13:10:55 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Routes from './Routes';
+import { Link } from "react-router-dom";
 
 function App() {
 
   return (
     <div className="App">
+      <h1><Link to='/cart'>CART</Link></h1>
+      <h1><Link to='/'>HOME</Link></h1>
       <Routes/>
     </div>
   );
