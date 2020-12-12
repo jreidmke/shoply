afc950ed34978e1c4815d23f352dd9096aad72ad
unknown <jreidmke@gmail.com>
2020-12-12 10:25:08 -0600

    refactoring to allow for use of combineReducers

---
+++ src/reducers/cart.js
@@ -0,0 +1,16 @@
+import {splicer} from './helpers';
+
+const INITIAL_STATE = { cart: [] };
+
+function cart(state=INITIAL_STATE, action) {
+    switch(action.type) {
+        case 'ADD_TO_CART':
+            return {...state, cart: [...state.cart, action.payload]}
+
+        case 'REMOVE_FROM_CART':
+            return {...state, cart: [...splicer(state.cart, action.id)]}
+
+        default:
+            return state;
+    }
+}
