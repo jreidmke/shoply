ab5e563c15ff235c2b79945fe0ee15fd6fca3d94
unknown <jreidmke@gmail.com>
2020-12-12 09:39:53 -0600

    included item count in navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,2 +1,17 @@
 import {NavLink} from 'react-router-dom';
-import './NavBar.css'; 
+import './NavBar.css';
+import {useSelector} from 'react-redux';
+
+function NavBar() {
+    const cart = useSelector(state => state.cart);
+
+    return(
+        <nav>
+            <NavLink exact to="/">Home</NavLink>
+            <NavLink exact to="/cart">Cart</NavLink>
+            <h2>Items in Cart: {cart.length}</h2>
+        </nav>
+    )
+}
+
+export default NavBar;
