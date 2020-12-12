3bac93f64cecffd43e953cb4793f36e22cb35e24
unknown <jreidmke@gmail.com>
2020-12-12 10:00:30 -0600

    added toatl cost

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,8 +1,9 @@
 import {NavLink} from 'react-router-dom';
 import './NavBar.css';
 import {useSelector} from 'react-redux';
+import {total} from './helpers';
 
-function NavBar() {
+const NavBar = () => {
     const cart = useSelector(state => state.cart);
 
     return(
@@ -10,6 +11,7 @@ function NavBar() {
             <NavLink exact to="/">Home</NavLink>
             <NavLink exact to="/cart">Cart</NavLink>
             <h2>Items in Cart: {cart.length}</h2>
+            <h2>Total Cost: {total(cart)}</h2>
         </nav>
     )
 }
