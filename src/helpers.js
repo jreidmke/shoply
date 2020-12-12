8ed62ed3281d7ee9b8a6fa9498cc64a85c1f9010
unknown <jreidmke@gmail.com>
2020-12-12 10:00:53 -0600

    moved helper functions to own module

---
+++ src/helpers.js
@@ -0,0 +1,18 @@
+import data from './data.json';
+
+function splicer(arr, id) {
+    arr.splice(arr.indexOf(id), 1);
+    return arr;
+}
+
+function total(cart) {
+    const priceArray = cart.map(code => data.products[code].price);
+    if(!priceArray) {
+        return 0;
+    } else {
+        return priceArray.reduce((a, b) => parseFloat(a+b), [0])
+    }
+}
+
+export {splicer};
+export {total};
