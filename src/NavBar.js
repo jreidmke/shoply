ef7955cfa01067bb5e09ca10cd0eb86c67044280
unknown <jreidmke@gmail.com>
2020-12-12 12:00:43 -0600

    structuring out total solution

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,10 +1,13 @@
 import {NavLink} from 'react-router-dom';
 import './NavBar.css';
 import {useSelector} from 'react-redux';
+import {useState} from 'react';
 import {total} from './helpers';
 
 const NavBar = () => {
     const cart = useSelector(state => state.cart.cart);
+    const [t, setTotal] = useState(total(cart));
+
 
     return(
         <nav>
@@ -12,6 +15,7 @@ const NavBar = () => {
             <NavLink exact to="/cart">Cart</NavLink>
             <h2>Items in Cart: {cart.length}</h2>
             <h2>Total Cost: {total(cart)}</h2>
+
         </nav>
     )
 }
